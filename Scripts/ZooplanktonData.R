03cafd9da2b9e22cd70ed9b789e509504c143357
Zeynep Yuksel <your_s2320459@ed.ac.uk>
2025-09-24 13:18:35 +0100

    cleaning data

--- Scripts/ZooplanktonData.R
+++ Scripts/ZooplanktonData.R
@@ -4,21 +4,31 @@ library(readr)
 library(readxl)
 
 # Step 1: Load the zooplankton dataset 
-zoop<- read_excel("Data/BATS_zooplankton (1).xlsx")
+zooplankton<- read_excel("Data/BATS_zooplankton (1).xlsx")
 View(BATS_zooplankton_1_)
 
 # Step 2: Check column names ---
-# You should have at least: Date, Depth, Biomass (adjust as needed)
-# Example: "yyyymmdd", "Depth", "Biomass_mg_m3"
-names(zoop)
+# Getting rid of the first 34 rows
+zooplankton<- zooplankton[-c(1:34), ]
+
+#making row 1 the column headers
+colnames(zooplankton) <- as.character(unlist(zooplankton[1, ]))
+names(zooplankton)
+
+zooplankton <- zooplankton %>%
+  mutate(Date = as.Date(as.character(date), format = "%Y%m%d"),
+         Depth = as.numeric(max_depth)) %>%
 
 # --- Step 3: Convert date to proper Date format ---
-zoop <- zoop %>%
-  mutate(Date = as.Date(as.character(yyyymmdd), format = "%Y%m%d"))
+#date is written as yyyymmdd e.g. 19940406
+
+zooplankton <- zooplankton %>%
+  mutate(Date = as.Date(as.character(date), format = "%Y%m%d"),
+         Depth = as.numeric(max_depth)) %>%
 
 # --- Step 4: Filter to only depths ≤ 200 m ---
-zoop_filtered <- zoop %>%
-  filter(Depth <= 200)
+# zoop_filtered <- zoop %>%
+#  filter(Depth <= 200)
 
 # --- Step 5: Depth integrate (sum biomass per sampling event) ---
 # Adjust biomass column name accordingly
@@ -31,3 +41,8 @@ zoop_integrated <- zoop_filtered %>%
 write_csv(zoop_integrated, "zooplankton_integrated_0_200m.csv")
 
 # Now zoop_integrated has one row per date with summed biomass for top 200 m
+
+# Visualising the data
+library(ggplot2)
+
+
